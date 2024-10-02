f1d48f922defbd4c870f87b1e4948d959b0d5e61
Aoife <mclouga5@tcd.ie>
2024-10-02 19:33:38 +0100

    show skeleton on api data loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider, useQuery,} from '@tanstack/react-query'
 import axios from "axios";
 import { format, parseISO, fromUnixTime } from "date-fns";
 import Container from "@/components/Container";
@@ -18,6 +14,9 @@ import { convertWindSpeed } from "@/utils/convertWindSpeed";
 import { metersToKilometers } from "@/utils/metersToKilometers";
 import ForecastWeatherDetail from "@/components/ForecastWeatherDetail";
 import { formatDateWithSuffix } from "@/utils/getOrdinalSuffix";
+import { loadingCityAtom, placeAtom } from "./atom";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 interface WeatherDetail {
   dt: number;
@@ -75,15 +74,22 @@ interface WeatherData {
 }
 
 export default function Home() {
-  const { isPending, error, data } = useQuery<WeatherData>({
+  const [place, setPlace] = useAtom(placeAtom);
+  const [loadingCity] = useAtom(loadingCityAtom);
+
+  const { isPending, error, data, refetch } = useQuery<WeatherData>({
     queryKey: ['repoData'],
     queryFn: async () => {
       const {data} = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=london&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
       );
       return data;
     }
   });
+
+  useEffect(() => {
+    refetch();
+  }, [place, refetch]);
 
   const todayData = data?.list[0];
 
@@ -113,11 +119,16 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar
+      location={data?.city.name}/>
 
       <main className="px-3 py-4 max-w-7xl mx-auto flex flex-col gap-9 w-full">
 
-      {/* Today's data*/}
+      {/* Today's data */}
+      {loadingCity ? (
+        <WeatherSkeleton />
+      ) : (
+        <>
       <section className="space-y-4">
         <div className="space-y-2">
           <h2 className="flex gap-1 text-2xl items-end">
@@ -237,8 +248,49 @@ export default function Home() {
                 />
               ))}
       </section>
+    </>
+    )}
+  </main>
+  </div>
+  );
+}
 
-      </main>
-    </div>
+function WeatherSkeleton() {
+  return (
+    <section className="space-y-8 ">
+      {/* Today's data skeleton */}
+      <div className="space-y-2 animate-pulse">
+        {/* Date skeleton */}
+        <div className="flex gap-1 text-2xl items-end ">
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+        </div>
+
+        {/* Time wise temperature skeleton */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {[1, 2, 3, 4].map((index) => (
+            <div key={index} className="flex flex-col items-center space-y-2">
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* 7 days forecast skeleton */}
+      <div className="flex flex-col gap-4 animate-pulse">
+        <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
+
+        {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+          <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
