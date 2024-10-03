4486f6b3e82045fdb319fed15d5d75232b63ed3d
Aoife <mclouga5@tcd.ie>
2024-10-03 13:38:46 +0100

    extend global atom to contain coordinates

--- src/app/atom.ts
+++ src/app/atom.ts
@@ -1,5 +1,11 @@
 import { atom } from "jotai";
 
-export const placeAtom = atom("london");
+export const placeAtom = atom({
+    city: "London",
+    coordinates: {
+      lat: 51.5074,
+      lon: -0.1278,
+    },
+  });
 
 export const loadingCityAtom = atom(false);
